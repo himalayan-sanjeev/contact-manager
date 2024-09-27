ea8c8d8f9dbf30213a2955132e21438e5f219396
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-09-27 22:50:20 +0545

    Add search scope in contact model to handle query filtering

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  # Add a search scope to handle query filtering
+  scope :search, ->(query) {
+    where("name ILIKE ? OR phone ILIKE ? OR email ILIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
+  }
 end
