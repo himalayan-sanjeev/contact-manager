c159bbc1bf77ea890a3856c5dc93e07b1013cb67
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-09-28 00:22:08 +0545

    Use custom pagination helper for cleaner design of paginated index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def bootstrap_paginate(current_page, total_pages)
+    content_tag :ul, class: 'pagination justify-content-center' do
+      # Previous Page Link
+      concat(content_tag(:li, class: "page-item #{'disabled' if current_page == 1}") do
+        link_to 'Previous', url_for(page: current_page - 1), class: 'page-link'
+      end)
+
+      # Page Number Links
+      (1..total_pages).each do |page|
+        concat(content_tag(:li, class: "page-item #{'active' if page == current_page}") do
+          link_to page, url_for(page: page), class: 'page-link'
+        end)
+      end
+
+      # Next Page Link
+      concat(content_tag(:li, class: "page-item #{'disabled' if current_page == total_pages}") do
+        link_to 'Next', url_for(page: current_page + 1), class: 'page-link'
+      end)
+    end
+  end
 end
