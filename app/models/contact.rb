0f651a7b717be1b78ed8cb5097c2366f40adbf43
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-09-28 00:40:46 +0545

    Add validations for contact model attributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ApplicationRecord
+  # Validating attributes for data integrity
+  validates :name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  validates :phone, presence: true, length: { is: 10 }, numericality: { only_integer: true }
+
   # Add a search scope to handle query filtering
   scope :search, ->(query) {
     where("name ILIKE ? OR phone ILIKE ? OR email ILIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
