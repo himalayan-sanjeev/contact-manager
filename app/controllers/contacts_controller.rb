b50ca1ca6eb6ffa1d846c1e0aafb40b6af62e970
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-09-27 21:47:50 +0545

    Setup controller, routes and basic views

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,58 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
+  # GET /contacts
+  def index
+    @contacts=Contact.order(created_at: :DESC)
+  end
+
+  # GET /contacts/:id
+  def show
+  end
+
+  # GET /contacts/new
+  def new
+    @contact = Contact.new
+  end
+
+  # POST /contacts
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to @contact, notice: 'Contact was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /contacts/:id/edit
+  def edit
+  end
+
+  # PATCH/PUT /contacts/:id
+  def update
+    if @contact.update(contact_params)
+      redirect_to @contact, notice: 'Contact was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /contacts/:id
+  def destroy
+    @contact.destroy
+    redirect_to contacts_path, notice: 'Contact was successfully deleted.'
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def contact_params
+    params.require(:contact).permit(:name, :phone, :email)
+  end
+end
