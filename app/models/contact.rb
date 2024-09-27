74d639312b3a0f599abfea76543ea91e98fcd312
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-09-28 01:12:22 +0545

    Use regex for better validation of emails

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,10 @@
 class Contact < ApplicationRecord
+  # Regular expression for email validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   # Validating attributes for data integrity
   validates :name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true
   validates :phone, presence: true, length: { is: 10 }, numericality: { only_integer: true }
 
   # Add a search scope to handle query filtering
