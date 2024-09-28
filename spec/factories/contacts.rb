48e7f5f1e1426c6ad4780146647000afe6806a2b
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-09-28 22:16:47 +0545

    Add a fixture image and update factory for image attribute

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     name { Faker::Name.unique.name }
     email { Faker::Internet.unique.email }
     phone { Faker::PhoneNumber.unique.cell_phone.gsub(/\D/, '')[-10..-1].to_i }
+  
+    after(:build) do |contact|
+      contact.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/sample_image.png')),
+        filename: 'sample_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
