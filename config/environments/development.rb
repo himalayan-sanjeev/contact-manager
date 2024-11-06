0c6d1f46f718907c2e00b0cbf2ea8586eff8b678
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-11-06 16:51:19 +0545

    Add pwa feature to the app   - Added manifest.json for app metadata and icons   - Implemented service worker to cache assets for offline use   - Updated application layout to register service worker and link manifest   - Configured app to work as an installable PWA with offline support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # Allow all Ngrok subdomains
+  config.hosts << /[a-z0-9\-]+\.ngrok-free\.app/
 end
